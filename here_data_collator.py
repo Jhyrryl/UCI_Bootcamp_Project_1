8c0c9aeab8f0986a642346ac10220e1b075de003
Mike Paddock <jhyrryl@gmail.com>
2019-10-01 14:09:42 -0700

    More logic to ensure a single entry per accident; massage dates to be sortable

--- here_data_collator.py
+++ here_data_collator.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import json
 
+import sys
 import os
 from pathlib import Path
 
@@ -9,6 +10,13 @@ base_path = Path(base_data_path)
 
 accident_ids = {}
 accidents = []
+
+def fixTime( time_str ):
+    dt = time_str.split(" ")
+    newTime = dt[0][-4:] + "/" + dt[0][0:-5] + " " + dt[1]
+    sys.stdout.write(newTime+"\n")
+    sys.stdout.flush()
+    return newTime
 
 state_paths = [x for x in base_path.iterdir() if x.is_dir()]
 for state in state_paths:
@@ -20,9 +28,24 @@ for state in state_paths:
             data = pd.read_json(file)
             for item in data['TRAFFIC_ITEMS']['TRAFFIC_ITEM']:
                 item_id = item['TRAFFIC_ITEM_ID']
-                if not item_id in accident_ids:
-                    accident_ids[item_id] = True
-                    accidents.append(item)
+                orig_id = item['ORIGINAL_TRAFFIC_ITEM_ID']
+                if not orig_id in accident_ids:
+                    accident_ids[orig_id] = []
+                accident_ids[orig_id].append(item)
+
+for item_id, incidents in accident_ids.items():
+    last = None
+    last_time = ''
+    for incident in incidents:
+        t = fixTime(incident['END_TIME'])
+        if t > last_time:
+            last = incident
+            last_time = t
+    last['END_TIME'] = t
+    last['START_TIME'] = fixTime(last['START_TIME'])
+    last['ENTRY_TIME'] = fixTime(last['ENTRY_TIME'])
+    last['RELATED_ITEM_COUNT'] = len(incidents)
+    accidents.append(last)
 
 print(f"{len(accidents)} found")
 
