f65848b2704be1e21665a9bc5800c527fe61de83
Mike Paddock <jhyrryl@gmail.com>
2019-10-05 19:26:51 -0700

    Added state data to CleanData

--- DataCleaner.py
+++ DataCleaner.py
@@ -8,6 +8,11 @@ Created on Mon Sep 30 19:05:55 2019
 import json
 import pandas as pd
 from datetime import datetime, timedelta
+from config import gkey
+import requests
+import time
+import sys
+
 # read file
 with open('Resources/accidents.json', 'r') as myfile:
     data=myfile.read()
@@ -60,5 +65,33 @@ summarydf=pd.DataFrame.from_dict({"ID":IDlist,
                                   "Duration_(seconds)":durationtoseconds,
                                   "Vehicle_response":vehiclecalled ,
                                   "Road_closure":roadclosed})
+states = []
+
+url = "https://maps.googleapis.com/maps/api/geocode/json"
+params = {'key': gkey}
+
+for ndx, row in summarydf.iterrows():
+    # print(row)
+    lat_lng = f"{row['Latitude']},{row['Longitude']}"
+    params['latlng'] = lat_lng
+    response = requests.get(url,params).json()
+    state = None
+    for component in response['results'][0]['address_components']:
+        # print(component)
+        for t in component['types']:
+            if t == "administrative_area_level_1":
+                state = component['long_name']
+                break
+        if state != None:
+            break
+
+    states.append(state)
+    sys.stdout.write(f"{ndx} - {state}\n")
+    sys.stdout.flush()
+    time.sleep(0.02)
+
+summarydf['State'] = states
     
 summarydf.to_json("Resources/CleanedData.json")
+
+
